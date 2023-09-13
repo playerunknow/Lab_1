627671dc413d4c68500e32c9fc9855815c198236
User <fghfhg@>
2023-09-13 10:51:27 +0300

    .

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 from random import randint
 
+
+def fill_array_with_random_numbers(arr, size):
+    for i in range(size):
+        arr.append(randint(1, 9))
+
+
+def add_arrays_with_carry(first_array, second_array):
+    temp = 0
+    max_size = max(len(first_array), len(second_array))
+
+    while len(first_array) < len(second_array):
+        first_array.insert(0, 0)
+    while len(second_array) < len(first_array):
+        second_array.insert(0, 0)
+
+    for i in range(max_size - 1, -1, -1):
+        first_digit = first_array[i] if i < len(first_array) else 0
+        second_digit = second_array[i] if i < len(second_array) else 0
+        total = first_digit + second_digit + temp
+        temp = total // 10
+        result_array.insert(0, total % 10)
+
+    if temp > 0:
+        result_array.insert(0, temp)
+
+    return result_array
+
+
 first_array = []
 second_array = []
 result_array = []
 
-for i in range(10):
-    first_array.append(randint(0, 9))
-    second_array.append(randint(0, 9))
+first_arr_size = int(input("Enter size of 1 arr: "))
+second_arr_size = int(input("Enter size of 2 arr: "))
 
-carry = 0
-for i in range(9, -1, -1):
-    total = first_array[i] + second_array[i] + carry
-    carry = total // 10
-    result_array.insert(0, total % 10)
+fill_array_with_random_numbers(first_array, first_arr_size)
+fill_array_with_random_numbers(second_array, second_arr_size)
 
-if carry > 0:
-    result_array.insert(0, carry)
-
+add_arrays_with_carry(first_array, second_array)
 
 print(f"First array: {first_array}")
 print(f"Second array:{second_array}")
